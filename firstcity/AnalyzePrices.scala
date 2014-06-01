242581b24b149b9cf4406530b56c5ca1e9474c7d
Jennifer Yip <lunchbag@gmail.com>
2014-06-01 14:46:52 -0700

    wrong file name

--- firstcity/AnalyzePrices.scala
+++ firstcity/AnalyzePrices.scala
@@ -2,14 +2,16 @@ import com.twitter.scalding._
 
 class AnalyzePrices(args : Args) extends Job(args) {
   TextLine( "../sensation/firstcity/dummy.txt" )
-    .map('line -> ('action, 'price)) {
+    .map('line -> ('date, 'action, 'price)) {
       line : String =>
         val split = line.split("""\s+""")
-        (split(0), split(1))
+        (split(0), split(1), split(2))
     }
-    .groupBy('action) {
+    .groupBy('date) {
       _.sizeAveStdev('price -> ('count, 'averagePrice, 'stdevPrice))
     }
     .write (Tsv( "../sensation/firstcity/output.tsv" ))
 }
+
+
 
--- generateDummyData.rb
+++ generateDummyData.rb
@@ -1,10 +1,23 @@
 #!/usr/bin/env ruby
 actions = ['selling', 'buying']
+dates = ['2014/06/01',
+               '2014/05/31',
+               '2014/05/30',
+               '2014/05/29',
+               '2014/05/28',
+               '2014/05/27',
+               '2014/05/26',
+               '2014/05/25',
+               '2014/05/24',
+               '2014/05/23',
+               '2014/05/22',
+               '2014/05/21',
+               '2014/05/20']
 
 def generateRandomCost(base)
   base + (rand(0..base) * (-0.1) ** rand(1..2))
 end
 
 for i in 0..1000
-  print [actions.sample, generateRandomCost(400)].join(" ") + "\n"
+  print [dates.sample, actions.sample, generateRandomCost(400)].join(" ") + "\n"
 end
